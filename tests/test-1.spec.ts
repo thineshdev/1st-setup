d709858674abdacf7f89dcedd71a6967c8b38ce1
Thinesh <t.thinesh@faaztechsolutions.com>
2025-03-09 14:21:25 +0530

    first commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  // Recording...
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('feed the dog ');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('water the plant');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('i do shoping');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+
+  await expect(page.getByTestId('todo-title')).toHaveCount(3);
+  await page.getByRole('listitem').filter({ hasText: 'feed the dog' }).getByLabel('Toggle Todo').check();
+  await page.getByRole('link', { name: 'Active' }).click();
+  await expect(page.getByTestId('todo-title')).toHaveCount(2);
+  await page.getByRole('link', { name: 'Completed' }).click();
+});
