551a09b1c84967afc784cb6fa7a140652094b76b
krishna2003 <m.krishna@faaztechsolutions.com>
2025-03-09 14:41:52 +0530

    check push

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
   await page.goto('https://demo.playwright.dev/todomvc/#/');
   await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
   await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('feed the dog ');
